e1f48697957503040185b36c2d74aa6346b31c28
gastonperego <gastonperego@holbertonstudents.com>
2023-09-03 20:17:06 +0000

    fix: betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,9 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
     if (!newnode)
-    {
         return(NULL);
-    }
     newnode->parent = parent;
     newnode->n = value;
     newnode->left = NULL;
