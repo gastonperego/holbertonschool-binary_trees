3e4713bae1e3c5f8dd00d39d909f95ce0fa3fe44
gastonperego <gastonperego@holbertonstudents.com>
2023-09-03 17:20:54 +0000

    feat: binary_tree_delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+* binary_tree_delete - deletes a binary tree
+*
+* @tree: pointer to the root of the tree
+*
+* Return: void
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+    binary_tree_t *tmp = tree;
+
+    if (!tree)
+        ;
+    else
+    {
+        binary_tree_delete(tmp->left);
+        binary_tree_delete(tmp->right);
+        free(tmp);
+    }
+}
