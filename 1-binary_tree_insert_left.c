3814375808ab616f9029e141f465b766c73141f7
gastonperego <gastonperego@holbertonstudents.com>
2023-09-03 15:51:45 +0000

    fix:

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,19 +6,24 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *newnode = NULL;
 
+    if (!parent)
+        return(NULL);
     newnode = malloc(sizeof(binary_tree_t));
-    if (newnode == NULL || parent == NULL)
+    if (newnode == NULL)
         return (NULL);
     newnode->parent = parent;
     newnode->n = value;
+    newnode->right = NULL;
     if (parent->left != NULL)
     {
         newnode->left = parent->left;
         parent->left = newnode;
-        newnode->left->parent = newnode;
+        (newnode->left)->parent = newnode;
     }
     else
+    {
+        newnode->left = NULL;
         parent->left = newnode;
-    
+    }
     return(newnode);
 }
