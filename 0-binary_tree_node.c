d964dc052c67d605a994dd7ff1caddd888afefd5
gastonperego <gastonperego@holbertonstudents.com>
2023-09-03 14:31:19 +0000

    fix: betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,15 +1,23 @@
 #include "binary_trees.h"
 /**
+* binary_tree_node - adds a node to a binary tree
 *
+* @parent: pointer to the parent node of the node to create
+* @value: value to put in the new node
+*
+* Return: pointer to the new node
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
-
+    if (!newnode)
+    {
+        return(NULL);
+    }
     newnode->parent = parent;
     newnode->n = value;
     newnode->left = NULL;
     newnode->right = NULL;
 
-    return(newnode);
+    return (newnode);
 }
