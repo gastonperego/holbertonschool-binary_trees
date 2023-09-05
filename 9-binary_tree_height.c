eff4193254082fe52e74301759c215e98b3634d0
Gastón <135636714+gastonperego@users.noreply.github.com>
2023-09-05 11:33:17 -0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int right = 0;
 	int left = 0;
 
-	if (!tree)
+	if (!tree || (!tree->right && !tree->left)
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
